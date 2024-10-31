bb48cc45f619fa15e2f3c8cc72e53cef6f12cdfa
Jimmy Feng Feng <jimmy.feng04@gmail.com>
2024-10-30 21:27:32 -0600

    [ADD] Colisiones y metodos para modificar variables

--- Source/Client/game.h
+++ Source/Client/game.h
@@ -6,6 +6,12 @@
 #define CLIENT_GAME_H
 
 #include <raylib.h>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <cjson/cJSON.h>
 
 typedef struct Player {
     Vector2 position;
@@ -36,7 +42,7 @@ typedef struct Brick {
     bool speedDown;
 } Brick;
 
-#define PLAYER_MAX_LIFE         3
+#define PLAYER_MAX_LIFE         1000
 #define MAX_BALLS              20
 #define LINES_OF_BRICKS         8
 #define BRICKS_PER_LINE        14
@@ -51,6 +57,8 @@ static Player player = { 0 };
 static Ball balls[MAX_BALLS] = { 0 };
 static Brick brick[LINES_OF_BRICKS][BRICKS_PER_LINE] = { 0 };
 static Vector2 brickSize = { 0 };
+static bool levelSpeedChanged[LINES_OF_BRICKS / 2] = {false};  // Hay dos líneas por nivel
+static int levelsCompleted = 0;
 
 void InitGame();         // Initialize game
 void UpdateGame();       // Update game (one frame)
@@ -58,5 +66,28 @@ void DrawGame();         // Draw game (one frame)
 void UnloadGame();       // Unload game
 void UpdateDrawFrame();  // Update and Draw (one frame)
 void start_game();       // Inicia el juego
+
+void check_brick(int i, int j);
+
+void update_brick_score(int level, int new_points);
+void update_player_score(int brickx, int bricky);
+
+void update_brick_ball(int i, int j);
+void add_ball(int i, int j);
+
+void update_brick_life(int i, int j);
+void add_life();
+
+void update_brick_doubleRacket(int i, int j);
+void doubleRacket();
+
+void update_brick_halfRacket(int i, int j);
+void halfRacket();
+
+void update_brick_speedUp(int i, int j);
+void speedUp();
+
+void update_brick_speedDown(int i, int j);
+void speedDown();
 
 #endif //CLIENT_GAME_H
